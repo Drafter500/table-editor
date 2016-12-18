6cdb03d9980e0564b4d62201afdec61ebff9ed9d
Drafter500 <vzemtsov@yahoo.com>
2016-12-18 23:10:55 +0500

    Update table edit object

--- jasmine/spec/tableSpec.js
+++ jasmine/spec/tableSpec.js
@@ -1,30 +1,31 @@
 describe("Table", function() {
-  // var table; 
+  var tableEdit;
   // var tableBody; 
   // var columnNames;
 
   beforeEach(function() {
-    window.table = $("<table>\
+    tableEdit = new TableEdit();
+    tableEdit.table = $("<table>\
     <thead><tr><th>h1</th><th>h2</th><th>h3</th></tr></thead>\
     <tbody><tr><td>1.1</td><td>1.2</td><td>1.3</td></tr>\
     <tr><td>2.1</td><td>2.2</td><td>2.3</td></tr>\
     <tr><td>3.1</td><td>3.2</td><td>3.3</td></tr></tbody>\
     </table>");
-    window.tableBody = table.find('tbody');  
-    window.columnNames = ['1', '2', '3'];
+   
     //TODO: Get rid of global variables, this is ugly
   });
 
   it("should return cell index", function() {
-    var cell22 = table.find("tr:nth-child(2)").find("td:nth-child(2)");
+    var cell22 = tableEdit.table.find("tr:nth-child(2)").find("td:nth-child(2)");
     expect(cell22.text()).toEqual("2.2");
-    var index = cellIndexForElement(cell22);
+    var index = tableEdit.cellIndexForElement(cell22);
   
     expect(index.row).toEqual(1);
     expect(index.col).toEqual(1);
   });  
 
   it("should add empty row", function() {
+    var tableBody = tableEdit.table.find('tbody');
     expect(tableBody.children("tr").length).toEqual(3);
     addEmptyRow();
     expect(tableBody.children("tr").length).toEqual(4);
--- js/table.js
+++ js/table.js
@@ -1,203 +1,199 @@
-function cellIndexForElement(el) {
-	return { row : $(el).closest('tr').index(),
-			 col : $(el).closest('td').index() }
-};
-
-function makeCellEditable(classSuffix, value, isNewItem = false) {
-	return '<input type="text" class="input-' + 
-	classSuffix + (isNewItem ? " new-item" : "") +
-		'" value="' + value + '">';
-}
-
-function activateEditingMode() {
-	tableBody.children('tr').each(function() {
-		var i = 0;
-		for(var i = 0; i < columnNames.length; i++) {
-			var td = $(this).children('td')[i];
-			var cellDiv = $(td).find('.cell');
-			var currentText = $(cellDiv).text();
-			$(cellDiv).html(makeCellEditable(
-				columnNames[i], currentText));				
-		}
-	});
-	addEmptyRow();
-	$('td input[type=text]').on('change', onInputChanged);
-	activateResizeMode();
-}
-
-function onInputChanged() {
-	var index = cellIndexForElement(this);
-	updateData(index.row, index.col, $(this).val());
-}
-
-function updateLocalStorage() {
-	localStorage['tableData'] = JSON.stringify(tableData);
-}
-
-function updateData(row, col, value) {
-	tableData[row][columnNames[col]] = value;
-	updateLocalStorage();
-}
-
-function addEmptyRow() {
-	var newRowText = '<tr>';
-	for (var i = 0; i < columnNames.length; i++) {
-		newRowText += "<td><div class=\"cell\">" + 
-		makeCellEditable(columnNames[i], '', true) +
-		"</div></td>";
+function TableEdit() {
+	function makeCellEditable(classSuffix, value, isNewItem = false) {
+		return '<input type="text" class="input-' + 
+		classSuffix + (isNewItem ? " new-item" : "") +
+			'" value="' + value + '">';
 	}
-	newRowText += "<td></td>"
-	newRowText += '</tr>'
-	tableBody.append(newRowText);
-	$('.new-item').on('change', newItemChanged);
-	$('.new-item').on('change', onInputChanged);		
-}
 
-function deactivateEditingMode() {
-	$('input[type=text]').off();
-	tableBody.children('tr').each(function() {
-		$(this).children('td').each(function() {
-			var currentText = $(this).find('input').val();
-			var cellDiv = $(this).find('.cell');
-			$(cellDiv).html(currentText);
+	function activateEditingMode() {
+		tableBody.children('tr').each(function() {
+			var i = 0;
+			for(var i = 0; i < columnNames.length; i++) {
+				var td = $(this).children('td')[i];
+				var cellDiv = $(td).find('.cell');
+				var currentText = $(cellDiv).text();
+				$(cellDiv).html(makeCellEditable(
+					columnNames[i], currentText));				
+			}
 		});
-	});
-	tableBody.children('tr').last().remove();
-	deactivateResizeMode();
-}
-
-function newItemChanged() {
-	var newItemDirty = false;
-	tableBody.find('.new-item').each(function(){
-		if ($(this).val != '') {
-			newItemDirty = true;
-			return false;				
-		}
-	});
-
-	if (newItemDirty) {
-		tableBody.find('.new-item').each(function() {
-			$(this).removeClass('new-item');
-			$(this).off('change', newItemChanged);			
-		});
-		tableBody.children('tr').last().children("td").last()
-		.html(deleteButton);				
-		
-		tableData.push({});
 		addEmptyRow();
+		$('td input[type=text]').on('change', onInputChanged);
+		activateResizeMode();
 	}
-}
 
-function populateTable(data) {
-	var tableText = "";
-	for(var i = 0; i < data.length; i++) {
-		tableText += "<tr>";
-		for (var j = 0; j < columnNames.length; j++) {
-			var cellValue = 
-				(data[i][columnNames[j]] !== undefined) ?
-				data[i][columnNames[j]] : "";
-			tableText += ("<td><div class=\"cell\">" + cellValue + "</div></td>");				
+	function onInputChanged() {
+		var index = self.cellIndexForElement(this);
+		updateData(index.row, index.col, $(this).val());
+	}
+
+	function updateLocalStorage() {
+		localStorage['tableData'] = JSON.stringify(tableData);
+	}
+
+	function updateData(row, col, value) {
+		tableData[row][columnNames[col]] = value;
+		updateLocalStorage();
+	}
+
+	function addEmptyRow() {
+		var newRowText = '<tr>';
+		for (var i = 0; i < columnNames.length; i++) {
+			newRowText += "<td><div class=\"cell\">" + 
+			makeCellEditable(columnNames[i], '', true) +
+			"</div></td>";
 		}
-		tableText += "<td><div class=\"cell\">" + deleteButton + "</div></td>";
-		tableText += "</tr>";
-	}		
-	tableBody.append(tableText);
-}
+		newRowText += "<td></td>"
+		newRowText += '</tr>'
+		tableBody.append(newRowText);
+		$('.new-item').on('change', newItemChanged);
+		$('.new-item').on('change', onInputChanged);		
+	}
 
-function connectDeleteButtons() {
-	$('tbody').on('click', '.delete-btn', function() {
-		deleteRow($(this).closest('tr').index());
-	});
-}
+	function deactivateEditingMode() {
+		$('input[type=text]').off();
+		tableBody.children('tr').each(function() {
+			$(this).children('td').each(function() {
+				var currentText = $(this).find('input').val();
+				var cellDiv = $(this).find('.cell');
+				$(cellDiv).html(currentText);
+			});
+		});
+		tableBody.children('tr').last().remove();
+		deactivateResizeMode();
+	}
 
-function deleteRow(row) {
-	tableBody.children('tr')[row].remove();
-	tableData.splice(row, 1);
-	updateLocalStorage();
-}	
+	function newItemChanged() {
+		var newItemDirty = false;
+		tableBody.find('.new-item').each(function(){
+			if ($(this).val != '') {
+				newItemDirty = true;
+				return false;				
+			}
+		});
 
-function getHeaderCell(resizer) {
-	var cell = $(resizer).closest('td');
-	var col = $(cell).index();
-	return table.find('th:nth-child(' + (col + 1)+ ')');
-}
+		if (newItemDirty) {
+			tableBody.find('.new-item').each(function() {
+				$(this).removeClass('new-item');
+				$(this).off('change', newItemChanged);			
+			});
+			tableBody.children('tr').last().children("td").last()
+			.html(deleteButton);				
+			
+			tableData.push({});
+			addEmptyRow();
+		}
+	}
 
-function columnCount() {
-	return tableBody.children("tr:first-child").children('td').length;
-}	
+	function populateTable(data) {
+		var tableText = "";
+		for(var i = 0; i < data.length; i++) {
+			tableText += "<tr>";
+			for (var j = 0; j < columnNames.length; j++) {
+				var cellValue = 
+					(data[i][columnNames[j]] !== undefined) ?
+					data[i][columnNames[j]] : "";
+				tableText += ("<td><div class=\"cell\">" + cellValue + "</div></td>");				
+			}
+			tableText += "<td><div class=\"cell\">" + deleteButton + "</div></td>";
+			tableText += "</tr>";
+		}		
+		tableBody.append(tableText);
+	}
 
-function resizeColumn(headerCell, cellWidth, nextCellWidth, tableWidth, difference) {
-	var col = $(headerCell).index();
-	$(headerCell).css('width', cellWidth + difference);
+	function connectDeleteButtons() {
+		$('tbody').on('click', '.delete-btn', function() {
+			deleteRow($(this).closest('tr').index());
+		});
+	}
 
-	if (col == columnCount() - 1) {			
-		table.css('width', tableWidth + difference);
+	function deleteRow(row) {
+		tableBody.children('tr')[row].remove();
+		tableData.splice(row, 1);
+		updateLocalStorage();
 	}	
 
-	if (columnCount() > 1 && (col < columnCount() - 1)) {	
-		$(headerCell).next('th').css('width', nextCellWidth - difference);
+	function getHeaderCell(resizer) {
+		var cell = $(resizer).closest('td');
+		var col = $(cell).index();
+		return self.table.find('th:nth-child(' + (col + 1)+ ')');
 	}
-}
 
-function getWidth(element) {
-	return parseInt(element.css('width'), 10);
-}
+	function columnCount() {
+		return tableBody.children("tr:first-child").children('td').length;
+	}	
 
-function activateResizeMode() {
-	tableBody.children('tr').each(function() {
-		$(this).children('td').each(function() {
-			var cellDiv = $(this).find('.cell');
-			$(cellDiv).append(resizerLine);				
-		});
-	});
+	function resizeColumn(headerCell, cellWidth, nextCellWidth, tableWidth, difference) {
+		var col = $(headerCell).index();
+		$(headerCell).css('width', cellWidth + difference);
 
-	$('.left-resizer').on('mouseover', function(){
-		$(this).css('cursor', 'ew-resize');
-	});
+		if (col == columnCount() - 1) {			
+			self.table.css('width', tableWidth + difference);
+		}	
 
-	var xBeforeMove;
-	var xAfterMove;
-	var borderCaptured = false;
-	var capturedHeaderCell;
-	var widthBeforeMove;
-	var nextWidthBeforeMove;
-	var tableWidthBeforeMove;
-	$('table')
-	.on('mousedown', '.left-resizer', function(e) {			
-		xBeforeMove = e.clientX;
-		borderCaptured = true;	
-		capturedHeaderCell = getHeaderCell(this);
-		widthBeforeMove = getWidth($(capturedHeaderCell));
-		nextWidthBeforeMove = getWidth($(capturedHeaderCell).next('th'));
-		tableWidthBeforeMove = getWidth(table);		
-		$(capturedHeaderCell).css('width', widthBeforeMove);		
-	})		
-
-	$(window).on('mousemove', function(e) {		
-		xAfterMove = e.clientX;
-		if (borderCaptured && (xAfterMove != xBeforeMove)) {				
-			var diff = xAfterMove - xBeforeMove;
-			resizeColumn(capturedHeaderCell, widthBeforeMove, nextWidthBeforeMove, tableWidthBeforeMove, diff);
+		if (columnCount() > 1 && (col < columnCount() - 1)) {	
+			$(headerCell).next('th').css('width', nextCellWidth - difference);
 		}
-	})
-	.on('mouseup', function() {
-		borderCaptured = false;
-	});
-}
+	}
 
-function deactivateResizeMode() {
-	tableBody.children('tr').each(function() {
-		$(this).children('td').each(function() {
-			$(this).find('.left-resizer').remove();							
+	function getWidth(element) {
+		return parseInt(element.css('width'), 10);
+	}
+
+	function activateResizeMode() {
+		tableBody.children('tr').each(function() {
+			$(this).children('td').each(function() {
+				var cellDiv = $(this).find('.cell');
+				$(cellDiv).append(resizerLine);				
+			});
 		});
-	});
-}
 
-$(document).ready(function() {
-	const table = $('table');
-	const tableBody = table.find('tbody');
+		$('.left-resizer').on('mouseover', function(){
+			$(this).css('cursor', 'ew-resize');
+		});
+
+		var xBeforeMove;
+		var xAfterMove;
+		var borderCaptured = false;
+		var capturedHeaderCell;
+		var widthBeforeMove;
+		var nextWidthBeforeMove;
+		var tableWidthBeforeMove;
+		$('table')
+		.on('mousedown', '.left-resizer', function(e) {			
+			xBeforeMove = e.clientX;
+			borderCaptured = true;	
+			capturedHeaderCell = getHeaderCell(this);
+			widthBeforeMove = getWidth($(capturedHeaderCell));
+			nextWidthBeforeMove = getWidth($(capturedHeaderCell).next('th'));
+			tableWidthBeforeMove = getWidth(self.table);		
+			$(capturedHeaderCell).css('width', widthBeforeMove);		
+		})		
+
+		$(window).on('mousemove', function(e) {		
+			xAfterMove = e.clientX;
+			if (borderCaptured && (xAfterMove != xBeforeMove)) {				
+				var diff = xAfterMove - xBeforeMove;
+				resizeColumn(capturedHeaderCell, widthBeforeMove, nextWidthBeforeMove, tableWidthBeforeMove, diff);
+			}
+		})
+		.on('mouseup', function() {
+			borderCaptured = false;
+		});
+	}
+
+	function deactivateResizeMode() {
+		tableBody.children('tr').each(function() {
+			$(this).children('td').each(function() {
+				$(this).find('.left-resizer').remove();							
+			});
+		});
+	}
+
+	var self = this;
+	this.table = $('table');
+	const tableBody = this.table.find('tbody');
 	const deleteButton = "<button class=\"delete-btn\">Delete</button>";
-	const resizerLine = "<div class=\"left-resizer\"></div>"
+	const resizerLine = "<div class=\"left-resizer\"></div>";
 	var columnNames = ['name', 'age', 'country'];
 	var tableData = [];
 	if (localStorage["tableData"]) {
@@ -222,20 +218,43 @@ $(document).ready(function() {
 		else {
 			deactivateEditingMode();
 		}
-	});	
+	});		
+}
 
-	$("#loader").on('click', function(event) {
-		event.preventDefault();
-		$.ajax('/content.html', {
-			type: "GET", 
-			success: function(result) {				
-				$('#content').html(result);
-			}
-		})
-	});
+TableEdit.prototype.cellIndexForElement = function(el) {
+	return { row : $(el).closest('tr').index(),
+			col : $(el).closest('td').index() }
+};
 
-	$("#loader2").on('click', function(event) {
-		event.preventDefault();
-		window.open('/content.html', "popup", 'height=300,width=300');
-	});
+$(document).ready(function() {
+	// const table = $('table');
+	// const tableBody = table.find('tbody');
+	// const deleteButton = "<button class=\"delete-btn\">Delete</button>";
+	// const resizerLine = "<div class=\"left-resizer\"></div>";
+	// var columnNames = ['name', 'age', 'country'];
+	// var tableData = [];
+	// if (localStorage["tableData"]) {
+	// 	tableData = JSON.parse(localStorage["tableData"]);
+	// } 
+	// else {
+	// 	tableData = 
+	// 	[{name : "John", age: 29, country : 'USA'},
+	//  	 {name : "Paul", age: 27, country : 'Germany'},
+	//  	 {name : "Vladimir", age: 26, country : 'Russia'}]
+	// }	
+
+	// populateTable(tableData);
+	// connectDeleteButtons();	
+
+	// // Firefox remembers form state after page refresh
+	// $('#edit-table-check').prop('checked', false);
+	// $('#edit-table-check').on('change', function() {
+	// 	if(this.checked) {
+	// 		activateEditingMode();
+	// 	}
+	// 	else {
+	// 		deactivateEditingMode();
+	// 	}
+	// });		
+	var tableEdit = new TableEdit();
 });
